8ecd6a099ba64d683827e7e64186aab5cd4900ba
Matienko Andrew <matienko1999@bk.ru>
2020-11-17 00:33:29 +0300

    Add files via upload

--- logreg_train.py
+++ logreg_train.py
@@ -6,6 +6,8 @@ from dslr.train_test_split import train_test_split
 from dslr.scaler import StandardScaler
 from dslr.model import LogisticRegression
 from dslr.stochastic_model import StochasticLogisticRegression
+from dslr.minibatch_logreg import MiniBatchLogisticRegression
+import numpy as np
 
 # print(pd.DataFrame(logreg._w))
 # print(logreg.predict_proba(X_train_scaled[:10]))
@@ -21,6 +23,7 @@ if __name__ == '__main__':
 	parser.add_argument('-l', '--loss', action="store_true", help="Show model")
 	parser.add_argument('-k', action="store_true", help="Loss function")
 	parser.add_argument('-s', action="store_true", help="Compare models")
+	parser.add_argument('-w', action="store_true", help="Models' weights")
 	args = parser.parse_args()
 
 	train = pd.read_csv(args.dataset)
@@ -37,19 +40,27 @@ if __name__ == '__main__':
 
 
 	y_train = y_train.reset_index(drop=True)
-	logreg = LogisticRegression()
+	y_test = y_test.reset_index(drop=True)
+	logreg = LogisticRegression(max_iter=1000)
 	logreg.fit(X_train_scaled, y_train)
 
-	slogreg = StochasticLogisticRegression()
+	##-- Stochastic Logistic Regression
+	slogreg = StochasticLogisticRegression(max_iter=1000)
 	slogreg.fit(X_train_scaled, y_train)
+
+	##-- Mini Batch Logistic Regression
+	minibatch = MiniBatchLogisticRegression(max_iter=1000)
+	minibatch.fit(X_train_scaled, y_train)
 
 	if (args.s):
 		plt.plot(logreg._loss.keys(), logreg._loss.values(), marker='o', c='red')
 		plt.plot(slogreg._loss.keys(), slogreg._loss.values(), marker='o', c='blue')
+		plt.plot(minibatch._loss.keys(), minibatch._loss.values(), marker='o', c='green')
 		plt.xlabel('step')
 		plt.ylabel('Loss')
 		plt.grid(True)
-		plt.legend(['LogisticRegression', 'StochasticLogisticRegression'])
+		plt.legend(['LogisticRegression', 'StochasticLogisticRegression',
+					'Mini Batch Logistic Regression'])
 		plt.show()
 
 
@@ -62,10 +73,33 @@ if __name__ == '__main__':
 
 	if (args.k):
 		print("LogisticRegression:")
+		print("\tFalse(Train):", sum([y_train[i] != logreg.predict(X_train_scaled)[i]
+										for i, _ in enumerate(y_train)]), "of", len(y_train))
+		print("\tFalse(Test):", sum([y_test[i] != logreg.predict(X_test_scaled)[i]
+							   for i, _ in enumerate(y_test)]), "of", len(y_test))
 		print("\tAccuracy score(Train):", accuracy_score(y_train, logreg.predict(X_train_scaled)))
 		print("\tAccuracy score(Test):", accuracy_score(y_test, logreg.predict(X_test_scaled)))
 		print("Stochastic LR")
+		print("\tFalse(Train):", sum([y_train[i] != slogreg.predict(X_train_scaled)[i]
+										for i, _ in enumerate(y_train)]), "of", len(y_train))
+		print("\tFalse(Test):", sum([y_test[i] != slogreg.predict(X_test_scaled)[i]
+							   for i, _ in enumerate(y_test)]), "of", len(y_test))
 		print("\tAccuracy score(Train):", accuracy_score(y_train, slogreg.predict(X_train_scaled)))
 		print("\tAccuracy score(Test):", accuracy_score(y_test, slogreg.predict(X_test_scaled)))
+		print("Mini Batch LR")
+		print("\tFalse(Train):", sum([y_train[i] != minibatch.predict(X_train_scaled)[i]
+										for i, _ in enumerate(y_train)]), "of", len(y_train))
+		print("\tFalse(Test):", sum([y_test[i] != minibatch.predict(X_test_scaled)[i]
+							   for i, _ in enumerate(y_test)]), "of", len(y_test))
+		print("\tAccuracy score(Train):", accuracy_score(y_train, minibatch.predict(X_train_scaled)))
+		print("\tAccuracy score(Test):", accuracy_score(y_test, minibatch.predict(X_test_scaled)))
+
+	if args.w:
+		print("Logistic Regression:")
+		print(pd.DataFrame(logreg._w, columns=np.append("Intercept", X_train.columns)))
+		print("Stochastic Logistic Regression:")
+		print(pd.DataFrame(slogreg._w, columns=np.append("Intercept", X_train.columns)))
+		print("Mini Batch Logistic Regression:")
+		print(pd.DataFrame(minibatch._w, columns=np.append("Intercept", X_train.columns)))
 
 	pd.DataFrame(logreg._w).to_csv('weights.csv', index=False)
---
+++ minibatch_logreg.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+class MiniBatchLogisticRegression:
+	def __init__(self, eta=0.1, max_iter=100, l2=0, initial_weights=None, multi_class=None):
+		self.eta = eta
+		self.max_iter = max_iter
+		self.l2 = l2
+		self._w = initial_weights
+		self._K = multi_class
+		self._errors = []
+		self._loss = {}
+		pass
+
+	def sigmoid(self, prediction):
+		return 1.0 / (1.0 + np.exp(-prediction))
+
+	def fit(self, X, y, sample_weight=None):
+		self._K = np.unique(y).tolist()
+		X_bias = np.c_[(np.ones((len(X), 1))), X]
+		m = 1 #X_bias.shape[0]
+
+		self._w = sample_weight
+		if not self._w:
+			self._w = np.zeros(X_bias.shape[1] * len(self._K))
+		self._w = self._w.reshape(len(self._K), X_bias.shape[1])
+
+		yVec = np.zeros((len(y), len(self._K)))
+
+		for i in range(len(y)):
+			yVec[i, self._K.index(y[i])] = 1
+
+		for i in range(self.max_iter + 1):
+			random = np.random.randint(low=len(X), size=32)[0]
+			y_pred = self.sigmoid(np.dot(self._w, X_bias[random].T))
+			loss_function = (-1.0 / m) * (np.sum((yVec[random].T * np.log(y_pred) + (1 - yVec[random].T) * np.log(1 - y_pred))))
+
+			k = yVec[random] - y_pred.T
+			gradients = -(1 / m) * np.dot((yVec[random] - y_pred.T).reshape(-1, 1), X_bias[random].reshape(1, -1))
+			step_size = self.eta * gradients
+			self._w = self._w - step_size
+			if i % 10 == 0:
+				self._loss[i] = loss_function
+				pass
+			pass
+		return self
+
+	def predict_proba(self, X):
+		X_bias = np.c_[(np.ones((len(X), 1))), X]
+		return self.sigmoid(np.dot(self._w, X_bias.T)).T
+
+	def predict(self, X):
+		probabilities = self.predict_proba(X)
+		return [self._K[x] for x in probabilities.argmax(1)]
